b358eb9e1c58723df269180db272f85f7b996e23
52254435 <52254435@COREPW0LX6YZ>
2026-08-05 17:27:07 +0530

    Fifth Commit

---
+++ Object Repository/TC-104/Page_Gmail Secure, AI-Powered Email for Eve_cdd15c/span_Create an account.rs
@@ -0,0 +1,114 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Create an account</name>
+   <tag></tag>
+   <elementGuidId>7af46e83-db4c-4abe-8641-f116e536b903</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.dropdown-gws-button.variant-primary.size-normal.align-left.valign-below > details > summary > span.button-label</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//section[@id='m3']/div/div/div/div[2]/div/dropdown-button-wrapper/div/details/summary/span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:text=&quot;Create an account&quot;i >> nth=2</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+      <webElementGuid>24b4d077-e820-4dfe-871f-d84d908b973f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>button-label</value>
+      <webElementGuid>fda249c8-6106-4ed4-83b1-edf8417498e3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Create an account</value>
+      <webElementGuid>6945a949-392a-412d-9e1d-2128b9788cc1</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;m3&quot;)/div[1]/div[@class=&quot;Grid_grid Template5050Hero_wrapper&quot;]/div[@class=&quot;Grid_column Template5050Hero_content Template5050Hero_fixedRightPadding&quot;]/div[@class=&quot;Text_container Text_body Text_lg Template5050Hero_body&quot;]/div[@class=&quot;ButtonGroup_buttonGroup ButtonGroup_buttonGroupHorizontal button-group-with-dropdown button-group horizontal&quot;]/dropdown-button-wrapper[@class=&quot;dropdown-wrapper&quot;]/div[@class=&quot;dropdown-gws-button variant-primary size-normal align-left valign-below&quot;]/details[1]/summary[1]/span[@class=&quot;button-label&quot;]</value>
+      <webElementGuid>e24c4959-cab2-4e3b-a322-7a16ccd0cf16</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//section[@id='m3']/div/div/div/div[2]/div/dropdown-button-wrapper/div/details/summary/span</value>
+      <webElementGuid>0cc77d14-9a18-41f4-aec7-cf696f17b252</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='AI-powered email for everyone'])[1]/following::span[1]</value>
+      <webElementGuid>4eafbf86-c657-4460-84fe-2a7ca708dc4e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Learn more'])[1]/following::span[1]</value>
+      <webElementGuid>029e5ce1-0cbe-4a22-b178-2ae10cefc6c8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='For my personal use'])[3]/preceding::span[3]</value>
+      <webElementGuid>60937be6-0de6-432d-a75f-577a3c128d5d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='For my work or business'])[3]/preceding::span[3]</value>
+      <webElementGuid>2f78f96c-c975-490f-aa11-42952ba0642c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/dropdown-button-wrapper/div/details/summary/span</value>
+      <webElementGuid>812ada67-6d4d-4e82-8cd8-25ad761a5b66</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//span[(text() = 'Create an account' or . = 'Create an account')]</value>
+      <webElementGuid>87d6e305-987e-4114-8462-0c4658b51481</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
